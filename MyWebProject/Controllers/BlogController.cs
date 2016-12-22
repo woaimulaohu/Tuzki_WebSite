7cf76f229b1f1545cf329e556204a792eb8c8bcc
Tuzki <Tuzki@TUZKI-PC>
2016-12-22 17:59:38 +0800

    未解决POST参数为HTML类型的问题

--- MyWebProject/Controllers/BlogController.cs
+++ MyWebProject/Controllers/BlogController.cs
@@ -1,9 +1,13 @@
 ﻿using MyWebProject.Models.Entity;
+using MyWebProject.Models.QueryResult;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 using System.Data.Entity.Core.EntityClient;
 using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,8 +22,54 @@ namespace MyWebProject.Controllers
 		{
 			return View();
 		}
+
+		/// <summary>
+		/// 获取摘要
+		/// </summary>
+		/// <returns></returns>
+		[ValidateInput(false)]
 		public ActionResult Snippet()
 		{
+			int pageStartNum = 0;
+			int pageSize = 5;
+			var aaa=Request["content"].ToString();
+			if (Request["pageStartNum"] != null && Request["pageSize"] != null)
+			{
+				int.TryParse(Request["pageStartNum"].ToString(), out pageStartNum);
+				int.TryParse(Request["pageSize"].ToString(), out pageStartNum);
+			}
+
+			List<SnippetResult> list = new List<SnippetResult>();
+			try
+			{
+				using (Entity entity = new Entity())
+				{
+					//conn.ConnectionString = "Server= 127.0.0.1\\SQLEXPRESS;DataDase= MyWebSite;user id=WebManager ;password= ;";
+					string sql = "SELECT " +
+				   "POST_CONTENT.POST_CONTENT," +
+				   "POST_INFO.DATE," +
+				   "POST_INFO.PRAISE_COUNT," +
+				   "POST_INFO.REPRODUCED_COUNT," +
+				   "POST_INFO.TITLE," +
+				   "POST_INFO.VIEW_COUNT " +
+				   "FROM " +
+				   "POST_INFO " +
+				   "JOIN POST_CONTENT ON POST_CONTENT.POST_ID = POST_INFO.POST_ID " +
+				   "AND POST_CONTENT.POST_ID IN({0},{1})";
+					var queryResult = entity.Database.SqlQuery<SnippetResult>(string.Format(sql, pageStartNum * pageSize, pageStartNum * pageSize + pageSize)).ToList();
+					foreach (SnippetResult p in queryResult)
+					{
+						p.POST_CONTENT = System.Web.HttpUtility.HtmlDecode(p.POST_CONTENT);
+						list.Add(p);
+					}
+					string a = JsonConvert.SerializeObject(queryResult);
+					return View(list);
+				}
+			}
+			catch (Exception ex)
+			{
+				logger.Error(ex);
+			}
 			return View();
 		}
 		public ActionResult Detial()
@@ -66,7 +116,7 @@ namespace MyWebProject.Controllers
 					//	logger.Info(p.TITLE);
 					//	list.Add(p);
 					//}
-
+					string a = JsonConvert.SerializeObject(reader);
 					return View(list);
 				}
 			}
---
+++ MyWebProject/Models/QueryResult/SnippetResult.cs
@@ -0,0 +1,24 @@
+﻿using MyWebProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebProject.Models.QueryResult
+{
+	public class SnippetResult
+	{
+		public string POST_CONTENT { get; set;}
+		public int POST_ID { get; set; }
+
+		public DateTime DATE { get; set; }
+
+		public string TITLE { get; set; }
+
+		public int? VIEW_COUNT { get; set; }
+
+		public int? REPRODUCED_COUNT { get; set; }
+
+		public int? PRAISE_COUNT { get; set; }
+	}
+}
