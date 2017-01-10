2a514df765cabef19f088fdcb0e2d00e0aa8ddce
Tuzki <Tuzki@TUZKI-PC>
2017-01-10 22:31:05 +0800

    增加github第三方登录后台逻辑代码,前端还没布局

--- MyWebProject/Controllers/OtherController.cs
+++ MyWebProject/Controllers/OtherController.cs
@@ -1,18 +1,61 @@
-﻿using System;
+﻿using MyWebProject.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace MyWebProject.Controllers
 {
-    public class OtherController : BaseController
+	public class OtherController : BaseController
 	{
-        // GET: Other
-        public ActionResult Index()
-        {
-			Response.RedirectToRoute(new { action = "Tips", controller = "Home", IsSuccess = false, Msg = "网站建设中,暂不支持此操作" });
+		log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+		// GET: Other
+		public ActionResult Index()
+		{
 			return View();
 		}
-    }
+		private string HttpGetResult(string reqUrl)
+		{
+			HttpWebRequest req = (HttpWebRequest)WebRequest.Create(reqUrl);
+			req.Method = "GET";
+			req.Timeout = 40 * 1000;
+			//必须加上UserAgent信息,值为Developer Settings-->OAuth Application-->Application name
+			req.UserAgent = "Personal web page";
+			req.ContentType = "application/x-www-form-urlencoded";
+			HttpWebResponse rsp = (HttpWebResponse)req.GetResponse();
+			// Get the stream associated with the response.
+			Stream receiveStream = rsp.GetResponseStream();
+			// Pipes the stream to a higher level stream reader with the required encoding format. 
+			return new StreamReader(receiveStream, Encoding.UTF8).ReadToEnd();
+		}
+
+		/// <summary>
+		/// 第三方登录方式github
+		/// </summary>
+		public void GitHubAccountLogin()
+		{
+			string code = Request["code"];
+			string step2Url = "https://github.com/login/oauth/access_token?client_id=1e8991731cc506dd7aa3&client_secret=b5e72132d8c5eadda8100e368ae2718907c5ecda&code=" + code;
+			string access_token = HttpGetResult(step2Url).Split('&')[0].Split('=')[1];
+			string step3Url = "https://api.github.com/user?access_token=" + access_token;
+			string json = HttpGetResult(step3Url);
+			GitHubUserInfo userInfo = JsonConvert.DeserializeObject<GitHubUserInfo>(json);
+			HttpCookie cookie = new HttpCookie("userInfo");
+			cookie.Values.Add("name", userInfo.name);
+			cookie.Values.Add("avatar_url", userInfo.avatar_url);
+			Response.SetCookie(cookie);
+#if DEBUG
+			Response.Redirect("http://localhost:7592/");
+#else
+			Response.Redirect("http://woaimulaohu.imwork.net/");
+#endif
+		}
+	}
 }
---
+++ MyWebProject/Models/GitHubUserInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebProject.Models
+{
+	public class GitHubUserInfo
+	{
+		public string login { get; set; }
+		public string id { get; set; }
+		public string avatar_url { get; set; }
+		public string gravatar_id { get; set; }
+		public string url { get; set; }
+		public string html_url { get; set; }
+		public string followers_url { get; set; }
+		public string following_url { get; set; }
+		public string gists_url { get; set; }
+		public string starred_url { get; set; }
+		public string subscriptions_url { get; set; }
+		public string organizations_url { get; set; }
+		public string repos_url { get; set; }
+		public string events_url { get; set; }
+		public string received_events_url { get; set; }
+		public string type { get; set; }
+		public string site_admin { get; set; }
+		public string name { get; set; }
+		public string company { get; set; }
+		public string blog { get; set; }
+		public string location { get; set; }
+		public string email { get; set; }
+		public string hireable { get; set; }
+		public string bio { get; set; }
+		public string public_repos { get; set; }
+		public string public_gists { get; set; }
+		public string followers { get; set; }
+		public string following { get; set; }
+		public string created_at { get; set; }
+		public string updated_at { get; set; }
+		public string private_gists { get; set; }
+		public string total_private_repos { get; set; }
+		public string owned_private_repos { get; set; }
+		public string disk_usage { get; set; }
+		public string collaborators { get; set; }
+		public plan Plan { get; set; }
+		public class plan
+		{
+			public string name { get; set; }
+			public string space { get; set; }
+			public string collaborators { get; set; }
+
+			public string private_repos { get; set; }
+		}
+	}
+}
