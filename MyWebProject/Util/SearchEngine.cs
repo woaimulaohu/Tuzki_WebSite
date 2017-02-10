862bbc74385e0855f6df72b2d92e6cdff1fb62f7
Tuzki <Tuzki@TUZKI-PC>
2017-02-10 18:28:30 +0800

    增加搜索引擎,刚加入客户端代码,服务端已测试完成.

---
+++ MyWebProject/Util/SearchEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SolrNet;
+
+namespace MyWebProject.Util_Pro
+{
+	public class SearchEngine
+	{
+		public static void Init()
+		{
+			SolrNet.Startup.Init
+		}
+	}
+}
